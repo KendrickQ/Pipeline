2f230f27896c0b652a61b5f4cd798432db7ee825
KendrickQ <42649623+KendrickQ@users.noreply.github.com>
2019-08-13 09:13:54 +0800

    Complete the instruction signals
    correct the zero signal

--- ALU.v
+++ ALU.v
@@ -21,15 +21,18 @@
 
 
 
-module ALU(in1, in2, ALUCtl, Sign, out, zero);
+module ALU(in1, in2, ALUCtl, OpCode_IDEX,
+     Sign, out, zero);
 	input [31:0] in1, in2;
 	input [4:0] ALUCtl;
+	input [5:0] OpCode_IDEX;
 	input Sign;
 	output reg [31:0] out;
 	output zero;
 	
-	assign zero = (out == 0);
-	
+//	assign zero = (out == 0);
+    assign zero = (OpCode_IDEX == 6'h04 & out == 0) ? 1 :(OpCode_IDEX == 6'h05 & out != 0)? 1 : 
+        (OpCode_IDEX == 6'h01 & out < 0) ? 1 : (OpCode_IDEX == 6'h06 & out <= 0) ? 1 : (OpCode_IDEX == 6'h07 & out>0) ? 1: 0;
 	wire ss;
 	assign ss = {in1[31], in2[31]};
 	
@@ -45,7 +48,7 @@ module ALU(in1, in2, ALUCtl, Sign, out, zero);
 			5'b00000: out <= in1 & in2;
 			5'b00001: out <= in1 | in2;
 			5'b00010: out <= in1 + in2;
-			5'b00110: out <= in1 - in2;
+			5'b00110: out <= in1 - in2;  // beq and sub
 			5'b00111: out <= {31'h00000000, Sign? lt_signed: (in1 < in2)};
 			5'b01100: out <= ~(in1 | in2);
 			5'b01101: out <= in1 ^ in2;
